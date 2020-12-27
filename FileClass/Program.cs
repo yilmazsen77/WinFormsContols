32415b7399b15827f1d64f05854dc9bca16011c8
Yılmaz Şen <yilmazsen77@outlook.com>
2020-12-27 22:58:42 +0300

    File class

---
+++ FileClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"";
+            string target = @"";
+
+
+            File.Create(source);
+            File.Copy(source, target);
+            File.Move(source, target);
+            File.Delete(source);
+
+            //string[] lines = { "","","" };
+            
+            string[] lines = File.ReadAllLines(target);
+            string text = "";
+
+            //File.WriteAllLines(target, lines);
+            File.WriteAllText(target, text);
+            foreach (var item in lines)
+            {
+                text += "\t" + item;
+            }
+
+            File.ReadAllText(target);
+            
+
+        }
+    }
+}
