350b8882b2b5b3f2c56dad48fd8c5369ab741d25
Yılmaz Şen <yilmazsen77@outlook.com>
2020-12-27 23:32:09 +0300

    CSharp property

---
+++ CSharpProperty/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.ProductName = "abc";
+            product.ProductCode = "1234";
+            product.ProductPrice = 7.77;
+
+            Console.WriteLine("Product Name: {0} - Code: {1} - Price: {2}", product.ProductName, product.ProductCode, product.ProductPrice);
+            Console.Read();
+        }
+    }
+
+    class Product
+    {
+        private string productName;
+        private string productCode;
+        private double productPrice;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string ProductCode
+        {
+            get { return productCode; }
+            set 
+            {
+                Random rnd = new Random();
+                productCode = value.ToString() + rnd.Next(100, 999).ToString();
+            
+            }
+        }
+
+        public double ProductPrice
+        {
+            get { return productPrice; }
+            set { productPrice = Math.Round(value, 1); }
+        }
+
+
+    }
+}
